ed618c0529c75cb0b7a5cd8bd1c9175eba578192
Shlok Arora <arorashlok2305@gmail.com>
2026-06-13 23:19:47 +0530

    Updated Insertion.java

--- src/No_1_Sorting/Insertion.java
+++ src/No_1_Sorting/Insertion.java
@@ -1,7 +1,7 @@
 package No_1_Sorting;
 
 public class Insertion {
-    public int[] insertionSort(int[] nums) {
+    public static int[] insertionSort(int[] nums) {
         for (int i = 0 ;i<=nums.length-1;i++){
             int j = i;
             while(j > 0 && nums[j-1] > nums[j]){
@@ -14,5 +14,11 @@ public class Insertion {
         return nums;
     }
     public static void main (String[]argd){
-        int []nums={7, 4, 1, 5, 3};
+        int [] nums={7, 4, 1, 5, 3};
+        int [] res = insertionSort(nums);
+        for(int i = 0 ; i<nums.length;i++){
+            System.out.print(res[i]+" ");
+        }
+    }
 }
+
